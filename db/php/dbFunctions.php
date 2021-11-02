cfd11194cef24c8990b518e983f04d775c6ff573
Jack <jfc33@njit.edu>
2021-11-02 18:02:02 -0400

    Error Logging Scripts and Skeleton for DB

---
+++ db/php/dbFunctions.php
@@ -0,0 +1,32 @@
+<?php
+require_once('../rabbitmqphp_example/path.inc');
+require_once('../rabbitmqphp_example/get_host_info.inc');
+require_once('../rabbitmqphp_example/rabbitMQLib.inc');
+require_once('rabbitMQClient.php');
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'off');
+ini_set('log_errors', 'On');
+ini_set('error_log', dirname(__FILE__).'/../logging/log.txt');
+
+function logAndSendErrors(){
+	$file = fopen("/../logging/log.txt", "r");
+	$errorArray = [];
+	while(!feof($file)){
+		array_push($errorArray, fgets($file));
+	}
+	fclose($file);
+
+	$request = array();
+	$request['type'] = "db";
+	$request['error_string'] = $errorArray;
+	$returnedValue = createClientForRmq($request);
+	
+	$fp = fopen("/../logging/logHistory.txt", "a");
+	for($i = 0; $i < count($errorArray); $i++){
+		fwrite($fp, $errorArray[$i]);
+	}
+
+	file_put_contents("/../logging/log.txt", "");
+}
+?>
---
+++ db/php/dbListener.php
@@ -0,0 +1,23 @@
+<?php
+require_once('../rabbitmqphp_example/path.inc');
+require_once('../rabbitmqphp_example/get_host_info.inc');
+require_once('../rabbitmqphp_example/rabbitMQLib.inc');
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+ini_set('log_errors', 'on');
+ini_set('error_log', dirname(__FILE__).'/../logging/log.txt');
+
+function requestProcessor($request){
+	echo "Received request".PHP_EOL;
+	echo $request['type'];
+	var_dump($request);
+	if(!isset($request['type'])){
+		return array('message'=>"ERROR: Message type is not supported");
+	}
+	echo "error";
+	$response_msg = logAndSendErrors();
+}
+$server = new rabbitMQServer('../rabbitmqphp_example/rabbitMQ_db.ini', 'testServer');
+$server->process_requests('requestProcessor');
+?>

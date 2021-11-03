83d75bc6ab3f853d4f596dddedeedf3726e6c4d6
Jansen Pimentel <jap264@njit.edu>
2021-11-03 16:26:32 -0400

    login stuff

--- db/php/dbFunctions.php
+++ db/php/dbFunctions.php
@@ -29,9 +29,69 @@ function logAndSendErrors(){
 
 	file_put_contents("/../logging/log.txt", "");
 }
-function  login($username, $password, $email){
-//unsure on pathing to query to
-//search for username, and then find/test corresponding matching password
-//salt/hash
+
+//user login
+function login($username, $password, $email){
+	
+	$connection = dbConnection();
+
+	$query = "SELECT * FROM user WHERE username = '$username'";
+	$result = $connection->query($query);
+
+	if($result){
+		if($result->num_rows == 0){
+			echo("No users in table.");
+			return false;
+		}
+		else {
+			while($row = $result->fetch_assoc()){
+				$salt = $row['salt'];
+				$h_password = generateHash($password,$salt);
+				if ($row['h_password'] == $h_password){
+					return true;
+				}
+				else{
+					return false;
+				}
+			}
+		}
+	}
+}
+
+//registers a new user
+function  register($username, $password, $email){
+
+	$connection = dbConnection();
+
+	$salt = generateSalt(29);
+
+	$h_password = generateHash($password,$salt);
+
+	$new_query = "INSERT INTO user VALUES ('$username','$email','$h_password','$salt');
+
+	$result = $connection->query($new_query);
+
+	return true;	
+}
+
+//hashes password to store in the db
+function generateHash($password, $salt) {
+	$new = $password . $salt;
+	return hash("sha256",$new);
+}
+
+//creates a salt for password
+function generateSalt($length) {
+	$string = '';
+	srand((double) microtime(TRUE) * 1000000);
+
+	$chars = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+
+	for($i = 0; $i <= $length; $i++) {
+		$rand = rand(0, count($chars) - 1);
+		$string .= $chars[$random];
+	}
+
+	return $string;
 }
 ?>
---
+++ frontend/php/loginReg.php
@@ -0,0 +1,70 @@
+<?php
+require_once(rabbitMQClient.php);
+
+if(isset($_POST["login"])) {
+	$username = null;
+	$password = null;
+	
+	if(isset($_POST["username"])){
+		$username = $_POST['username'];
+	}
+	if(isset($_POST["password"])){
+		$password = $_POST['password'];
+	}
+
+
+}
+
+if (isset($_POST["register"])) {
+    $email = null;
+    $password = null;
+    $confirm = null;
+    $username = null;
+    if (isset($_POST["email"])) {
+        $email = $_POST["email"];
+    }
+    if (isset($_POST["password"])) {
+        $password = $_POST["password"];
+    }
+    if (isset($_POST["password2"])) {
+        $confirm = $_POST["password2"];
+    }
+    if (isset($_POST["username"])) {
+        $username = $_POST["username"];
+    }
+    $isValid = true;
+    //check if passwords match on the server side
+    if ($password == $confirm) {
+        //not necessary to show
+        //echo "Passwords match <br>";
+    }
+    else {
+        echo("Passwords don't match");
+        $isValid = false;
+    }
+    if (!isset($email) || !isset($password) || !isset($confirm)) {
+        $isValid = false;
+    }
+    //TODO other validation as desired, remember this is the last line of defense
+    if ($isValid) {
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+    if($_SESSION == null){
+    	session_start();
+    }
+    $request = array();
+    $request['type'] = "register";
+    $request['username'] = $username;
+    $request['password'] = $password;
+    $request['email'] = $email;
+    $response = createClientForDb($request);
+    if($respone == 1) {
+    	$_SESSION["username"] = $username;
+	$_SESSION["register"] = true;
+    }
+    else{
+    	session_destroy();
+    }
+    echo $response;
+    return $respone;
+    }
+?>

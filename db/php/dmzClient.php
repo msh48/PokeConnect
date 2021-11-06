e93ddb066137506a9b96f213352e2e0d0b445f6e
Chanales <cf258@njit.edu>
2021-11-06 18:29:28 -0400

    quick fix

--- db/php/dmzClient.php
+++ db/php/dmzClient.php
@@ -39,4 +39,7 @@ $pokemon = createClientForDmz($request);
 //$manage = json_decode($pokemon, true);
 //$h = $manage["grookey"]["types"];
 $typ = gettype($pokemon);
+$data = json_decode($pokemon, TRUE);
+$typ2 = gettype($data)
 echo $typ.PHP_EOL;
+echo $typ2.PHP_EOL;

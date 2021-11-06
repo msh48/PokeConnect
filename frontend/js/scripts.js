62b9610cb01c4ea9ca95eae873de9c2c4033e75a
Jack <jfc33@njit.edu>
2021-11-06 15:58:32 -0400

    login touch-up

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,51 +1,22 @@
 function validateLogin(){
     
-    var loginUsernameNotClean = document.getElementById('loginUsername').value;
-    var loginPasswordNotClean = document.getElementById('loginPassword').value;
-    
-    var loginUsername = loginUsernameNotClean.trim();
-    var loginPassword = loginPasswordNotClean.trim();
+    var username = document.getElementById('loginUsername').value;
+    var password = document.getElementById('loginPassword').value;
 
-    if (loginUsername != "" && loginPassword != ""){
-        //sendLoginCredentials(loginUsername, loginPassword);
-        //alert("Login fields have be entered");
-        sendLoginCredentials(loginUsername, loginPassword);
-    }else{
-        if(loginUsername == ""){
-            turnFieldToRedColorBorder(loginUsername);
-            alert("Login fields have be entered");
-        }
-        if(loginPassword == ""){
-            turnFieldToRedColorBorder(loginPassword);
-            alert("Login fields have be entered");
-        }
-        if (loginUsername == "" && loginPassword == ""){
-            turnFieldToRedColorBorder(loginUsername);
-            turnFieldToRedColorBorder(loginPassword);
-            alert("Login fields have be entered");
-        }
-    }
+    sendLoginCredentials(username, password);
 }
 
-// This function sends a AJAX request for login 
 function sendLoginCredentials(username, password){
     
-    var httpReq = new XMLHttpRequest();
-    httpReq.onreadystatechange = function(){
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            
-            document.getElementById("loginButtonId").innerHTML = "Login";
             location.href = "boothomepage.html"
         }else{
 
-            document.getElementById("loginButtonId").innerHTML = "Loading...";
+            document.getElementById("loginButtonId").innerHTML = "Loading";
         }
     };
-    httpReq.open("GET", "../php/testLogin.php?username=" + username + "&password=" + password, true);
-    httpReq.send();
-}
-
-//  This function will add is-invalid to the division  
-function turnFieldToRedColorBorder(elementName){
-    elementName.classList.add("is-invalid");
+    http.open("GET", "../php/testLogin.php?username=" + username + "&password=" + password);
+    http.send();
 }
--- frontend/php/testLogin.php
+++ frontend/php/testLogin.php
@@ -1,7 +1,20 @@
 <?php
+error_reporting(E_ALL);
+
+ini_set('display_errors', 'Off');
+ini_set('log_errors', 'On');
+ini_set('error_log', dirname(__FILE__).'/../logging/log.txt');
+
+require_once('../rabbitmqphp_example/path.inc');
+require_once('../rabbitmqphp_example/get_host_info.inc');
+require_once('../rabbitmqphp_example/rabbitMQLib.inc');
 require_once("rabbitMQClient.php");
-$username = $REQUEST_['username'];
-$password = $REQUEST_['password'];
+
+if(session_status() != PHP_SESSION_ACTIVE){
+	session_start();
+}
+$username = $_GET['username'];
+$password = $_GET['password'];
 $request = array();
 $request['type'] = "login";
 $request['username'] = $username;

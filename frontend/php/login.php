cd9e8a7f2dc9b33b39c2f5773f601b4e9d262d6f
Jansen Pimentel <jap264@njit.edu>
2021-11-10 17:32:57 -0500

    added feedback for reg/login success/failure

--- frontend/php/login.php
+++ frontend/php/login.php
@@ -7,6 +7,7 @@ require_once('../rabbitmqphp_example/path.inc');
 require_once('../rabbitmqphp_example/get_host_info.inc');
 require_once('../rabbitmqphp_example/rabbitMQLib.inc');
 require_once('rabbitMQClient.php');
+require_once('../event_logging/event_logger.php');
 
 $client = new rabbitMQClient("../rabbitmqphp_example/rabbitMQ_db.ini","testServer");
 
@@ -16,6 +17,22 @@ $request['username'] = $_POST["username"];
 $request['password'] = $_POST["password"];
 $response = $client->send_request($request);
 
-session_destroy();
+if($response == 1){
+        $event = date("Y-m-d") . "  " . date("h:i:sa") . " [ FE ] " . "SUCCESS: login successful using Username = " . $_POST["username"] . " and Password = " . $_POST["password"] . "\n";
+        log_event($event);
+        /*$user = $_POST['username'];
+        $email = $_POST['email'];
+        $output = shell_exec("python3 emailscript.py $usr $email");*/
+        header("Location: www.testpoke.com/frontend/html/boothomepage.html");
+        exit();
+} else {
+        $error = date("Y-m-d") . "  " . date("h:i:sa") . " [ FE ] " . "ERROR: failed to login using Username = " . $_POST["username"] . " and Password = " . $_POST["password"] . "\n";
+        log_event($error);
+        //session_destroy();
+        header("Location: www.testpoke.com/frontend/html/login_failure.html");
+        exit();
+}
+
+//session_destroy();
 exit(0);
 }
--- frontend/php/register.php
+++ frontend/php/register.php
@@ -7,6 +7,7 @@ require_once('../rabbitmqphp_example/path.inc');
 require_once('../rabbitmqphp_example/get_host_info.inc');
 require_once('../rabbitmqphp_example/rabbitMQLib.inc');
 require_once('rabbitMQClient.php');
+require_once('../event_logging/event_logger.php');
 
 $client = new rabbitMQClient("../rabbitmqphp_example/rabbitMQ_db.ini","testServer");
 
@@ -18,6 +19,23 @@ $request['password'] = $_POST["password"];
 $request['password2'] = $_POST["password2"];
 $response = $client->send_request($request);
 
-session_destroy();
+if($response == 1){
+	$event = date("Y-m-d") . "  " . date("h:i:sa") . " [ FE ] " . "SUCCESS: registration successful using Username = " . $_POST["username"] . " and Password = " . $_POST["password"] . "\n";
+	log_event($event);
+	/*$user = $_POST['username'];
+	$email = $_POST['email'];
+	$output = shell_exec("python3 emailscript.py $usr $email");*/
+	header("Location: www.testpoke.com/frontend/html/reg_success.html");
+	exit();
+} else {
+	$error = date("Y-m-d") . "  " . date("h:i:sa") . " [ FE ] " . "ERROR: failed to register using Username = " . $_POST["username"] . " and Password = " . $_POST["password"] . "\n";
+	log_event($error);
+	//session_destroy();
+	header("Location: www.testpoke.com/frontend/html/reg_failure.html");
+	exit();
+}
+
+//session_destroy();
 exit(0);
 }
+?>

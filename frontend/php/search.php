f08461839466d13ed255c47f1cf993507580c7d0
Jansen Pimentel <jap264@njit.edu>
2021-11-10 20:15:41 -0500

    search debugging

--- frontend/php/search.php
+++ frontend/php/search.php
@@ -1,46 +1,39 @@
 <?php
-ini_set('display_errors', 1);
+session_start();
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
 require_once('../rabbitmqphp_example/path.inc');
 require_once('../rabbitmqphp_example/get_host_info.inc');
 require_once('../rabbitmqphp_example/rabbitMQLib.inc');
 require_once('rabbitMQClient.php');
+require_once('../event_logging/event_logger.php');
 
-if (!isset($_POST)) {
-	echo "No Post";
-	exit(0);
-}
+$client = new rabbitMQClient("../rabbitmqphp_example/rabbitMQ_db.ini","testServer");
 
-$type = $_POST["type"];
-switch ($type){    
-case "search":
+$request = array();
+$request['type'] = "register";
+$request['username'] = $_POST["pokemon"];
 
-    /*$isValid = true;
-    if ($password != $confirm) {
-        echo("Passwords don't match");
-        $isValid = false;
-    }
-    if (!isset($email) || !isset($password) || !isset($confirm) || !isset($username)) {
-        $isValid = false;
-    }*/
-    if($_SESSION == null){
-        session_start();
-    }
-    $request = array();
-    $request['type'] = "search";
-    $request['input'] = $_POST["pokemon"];
-    $response = createClientForDb($request);
-    if($response == 1) {
-        $_SESSION["input"] = $_POST["pokemon"];
-    echo "searched";
-    }
-    else{
-    echo "this did not work";
-        session_destroy();
-    }
-    echo json_encode($response);
-    //$jsonresp = json_encode($response);
-    //return  $jsonresp;
-    exit(0);
-    break;
+$response = $client->send_request($request);
+
+/*if($response == 0){
+	//$event = date("Y-m-d") . "  " . date("h:i:sa") . " [ FE ] " . "SUCCESS:  = " . $_POST["pokemon"]."\n";
+	//log_event($event);
+	/*$user = $_POST['username'];
+	$email = $_POST['email'];
+	$output = shell_exec("python3 emailscript.py $usr $email");*/
+	//header("Location: ../html/reg_success.html");
+	exit();
+} else {
+	$error = date("Y-m-d") . "  " . date("h:i:sa") . " [ FE ] " . "ERROR: failed to register using Username = " . $_POST["username"] . " and Password = " . $_POST["password"] . "\n";
+	log_event($error);
+	//session_destroy();
+	header("Location: ../html/reg_failure.html");
+	exit();
+}*/
+
+session_destroy();
+exit(0);
 }
 ?>
